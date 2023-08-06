5d782106a186885cb01cdd4b9672aa5682412ee6
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-08-06 10:18:10 +0200

    Update JustEnoughBreeding.java

--- Forge/src/main/java/com/christofmeg/justenoughbreeding/JustEnoughBreeding.java
+++ Forge/src/main/java/com/christofmeg/justenoughbreeding/JustEnoughBreeding.java
@@ -1,13 +1,10 @@
 package com.christofmeg.justenoughbreeding;
 
 import com.christofmeg.justenoughbreeding.config.JEBConfig;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
 import java.util.Map;
