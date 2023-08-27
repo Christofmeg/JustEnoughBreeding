f008b8a2759e219126c251c30e340466fdcc3fce
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-08-27 15:03:43 +0200

    Dynamic category creation
    * Changed to dynamic category texturing, where all textures are based on textures provided by JEI
    * zh_cn.json

--- Fabric/src/main/java/com/christofmeg/justenoughbreeding/jei/BreedingCategory.java
+++ Fabric/src/main/java/com/christofmeg/justenoughbreeding/jei/BreedingCategory.java
@@ -52,17 +52,19 @@ public class BreedingCategory implements IRecipeCategory<BreedingCategory.Breedi
     final ResourceLocation slotVanilla = new ResourceLocation("jei",
             "textures/jei/atlas/gui/slot.png");
 
-    final ResourceLocation breedingSlot = new ResourceLocation("justenoughbreeding",
-            "textures/gui/breeding.png");
-
-    final ResourceLocation eggSlot = new ResourceLocation("jei",
+    final ResourceLocation guiVanilla = new ResourceLocation("jei",
             "textures/jei/gui/gui_vanilla.png");
 
     private final IDrawable background;
     private final IDrawable icon;
     private final IDrawable slot;
-    private final IDrawable mobRenderSlot;
     private final IDrawable outputSlot;
+    private final IDrawable mobRenderSlotTop;
+    private final IDrawable mobRenderSlotBottom;
+    private final IDrawable mobRenderSlotLeft;
+    private final IDrawable mobRenderSlotRight;
+    private final IDrawable mobRenderSlotTopCorner;
+    private final IDrawable mobRenderSlotTopCenter;
 
     private final int breedableFoodSlotX = 69; //The hover slot
     private final int breedableFoodSlotY = 58; //The hover slot
@@ -71,8 +73,13 @@ public class BreedingCategory implements IRecipeCategory<BreedingCategory.Breedi
         background = helper.createBlankDrawable(166, 91);
         icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(itemStack));
         slot = helper.drawableBuilder(slotVanilla, 0, 0, 18, 18).setTextureSize(18, 18).build();
-        mobRenderSlot = helper.drawableBuilder(breedingSlot, 1, 13, 61, 81).setTextureSize(256,256).build();
-        outputSlot = helper.drawableBuilder(eggSlot, 25, 224, 57, 26).setTextureSize(256,256).build();
+        outputSlot = helper.drawableBuilder(guiVanilla, 25, 224, 57, 26).setTextureSize(256,256).build();
+        mobRenderSlotTop = helper.drawableBuilder(guiVanilla, 56, 128, 25, 1).setTextureSize(256, 256).build();
+        mobRenderSlotBottom = helper.drawableBuilder(guiVanilla, 57, 153, 25, 1).setTextureSize(256, 256).build();
+        mobRenderSlotLeft = helper.drawableBuilder(guiVanilla, 56, 129, 1, 24).setTextureSize(256, 256).build();
+        mobRenderSlotRight = helper.drawableBuilder(guiVanilla, 81, 129, 1, 24).setTextureSize(256, 256).build();
+        mobRenderSlotTopCorner = helper.drawableBuilder(guiVanilla, 81, 128, 1, 1).setTextureSize(256, 256).build();
+        mobRenderSlotTopCenter = helper.drawableBuilder(guiVanilla, 57, 129, 24, 24).setTextureSize(256, 256).build();
     }
 
     @Override
@@ -119,7 +126,40 @@ public class BreedingCategory implements IRecipeCategory<BreedingCategory.Breedi
 
         // output slot
         outputSlot.draw(stack, 94, 43);
-        mobRenderSlot.draw(stack, 0, 10);
+
+        mobRenderSlotTop.draw(stack, 0, 10);
+        mobRenderSlotTop.draw(stack, 25, 10);
+        mobRenderSlotTop.draw(stack, 35, 10);
+
+        mobRenderSlotTopCorner.draw(stack, 60, 10);
+        mobRenderSlotTopCorner.draw(stack, 0, 90);
+
+        mobRenderSlotBottom.draw(stack, 1, 90);
+        mobRenderSlotBottom.draw(stack, 26, 90);
+        mobRenderSlotBottom.draw(stack, 36, 90);
+
+        mobRenderSlotLeft.draw(stack, 0, 11);
+        mobRenderSlotLeft.draw(stack, 0, 35);
+        mobRenderSlotLeft.draw(stack, 0, 59);
+        mobRenderSlotLeft.draw(stack, 0, 66);
+
+        mobRenderSlotRight.draw(stack, 60, 11);
+        mobRenderSlotRight.draw(stack, 60, 35);
+        mobRenderSlotRight.draw(stack, 60, 59);
+        mobRenderSlotRight.draw(stack, 60, 66);
+
+        mobRenderSlotTopCenter.draw(stack, 1, 11);
+        mobRenderSlotTopCenter.draw(stack, 25, 11);
+        mobRenderSlotTopCenter.draw(stack, 36, 11);
+        mobRenderSlotTopCenter.draw(stack, 1, 35);
+        mobRenderSlotTopCenter.draw(stack, 25, 35);
+        mobRenderSlotTopCenter.draw(stack, 36, 35);
+        mobRenderSlotTopCenter.draw(stack, 1, 59);
+        mobRenderSlotTopCenter.draw(stack, 25, 59);
+        mobRenderSlotTopCenter.draw(stack, 36, 59);
+        mobRenderSlotTopCenter.draw(stack, 1, 66);
+        mobRenderSlotTopCenter.draw(stack, 25, 66);
+        mobRenderSlotTopCenter.draw(stack, 36, 66);
 
         EntityType<?> entityType = recipe.entityType;
         if(entityType != null) {
