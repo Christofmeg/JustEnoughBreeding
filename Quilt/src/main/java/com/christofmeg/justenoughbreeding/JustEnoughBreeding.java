1f81722ec0cb65f07b897d902051a6984bfbb982
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-10-19 11:47:28 +0200

    Change to true Quilt

--- Quilt/src/main/java/com/christofmeg/justenoughbreeding/JustEnoughBreeding.java
+++ Quilt/src/main/java/com/christofmeg/justenoughbreeding/JustEnoughBreeding.java
@@ -1,7 +1,8 @@
 package com.christofmeg.justenoughbreeding;
 
 import com.christofmeg.justenoughbreeding.config.JEBConfig;
-import net.fabricmc.api.ClientModInitializer;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -16,7 +17,7 @@ public class JustEnoughBreeding implements ClientModInitializer {
     public static final JEBConfig GENERAL = new JEBConfig(BUILDER);
 
     @Override
-    public void onInitializeClient() {
+    public void onInitializeClient(ModContainer mod) {
         ModLoadingContext.registerConfig(CommonConstants.MOD_ID, ModConfig.Type.CLIENT, BUILDER.build());
     }
 
