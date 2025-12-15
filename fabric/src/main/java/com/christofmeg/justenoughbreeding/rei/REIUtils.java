3907e98dda15f0b05b0e3565f98f7ebf0dbc7fa0
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2025-12-15 07:07:39 +0100

    Fabric net.fabricmc.unpick

--- fabric/src/main/java/com/christofmeg/justenoughbreeding/rei/REIUtils.java
+++ fabric/src/main/java/com/christofmeg/justenoughbreeding/rei/REIUtils.java
@@ -5,8 +5,6 @@ import com.christofmeg.justenoughbreeding.JustEnoughBreeding;
 import com.christofmeg.justenoughbreeding.recipe.BreedingRecipe;
 import com.christofmeg.justenoughbreeding.utils.Utils;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
@@ -41,8 +39,7 @@ public class REIUtils {
                                 Item spawnEggItem = JustEnoughBreeding.getItemFromLoaderRegistries(ResourceLocation.parse(mobSpawnEgg.trim()));
 
                                 if (spawnEggItem instanceof SpawnEggItem spawnEgg) {
-                                    HolderLookup.Provider holderProvider = Minecraft.getInstance().level.registryAccess();
-                                    EntityType<?> entityType = spawnEgg.getType(holderProvider, spawnEggItem.getDefaultInstance());
+                                    EntityType<?> entityType = spawnEgg.getType(spawnEggItem.getDefaultInstance());
                                     Boolean needsToBeTamed = CommonConstants.breedingNeedsToBeTamed.get(mobName);
                                     Boolean animalTrusting = CommonConstants.breedingNeedsToBeTrusting.get(mobName);
 
